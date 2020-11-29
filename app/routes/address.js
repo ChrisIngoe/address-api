f03ac7324581e679a0749558d45f3df7c2e2d9a8
ChrisIngoe <chris.software.engineer@gmail.com>
2020-11-29 19:00:54 +0000

    Add project configuration files

--- app/routes/address.js
+++ app/routes/address.js
@@ -1,10 +1,11 @@
 'use strict';
 
-const axios = require('axios');
+const axios = require('axios'),
+  config = require('config');
 
 exports.postcodeSearch = function (req, res) {
   const postcode = req.params.postcode;
-  axios.get(`https://postcodes.io/postcodes/${postcode}`)
+  axios.get(`${config.get('postcode-service.url')}/${postcode}`)
   .then(function (response) {
     return res.status(200).json( response.data.result );
   })
